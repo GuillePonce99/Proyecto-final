21183fd3c251dafeca90270036ccc3d152bcc092
Guillermo Ponce de Leon <guille.13577@gmail.com>
2023-11-30 10:50:14 -0300

    fixes

--- src/app.js
+++ src/app.js
@@ -40,7 +40,7 @@ const specs = swaggerJsdoc(swaggerOptions)
 //CLUSTER
 
 const numeroDeProcesadores = cpus().length
-
+/*
 if (cluster.isPrimary) {
 
     console.log("Proceso primario, generando worker");
@@ -53,89 +53,90 @@ if (cluster.isPrimary) {
         cluster.fork()
     })
 
-} else {
-
-    const app = express()
-
-    const environment = async () => {
-        await mongoose.connect(`mongodb+srv://${Config.MONGO_USER}:${Config.MONGO_PASSWORD}@coder.amwd2xp.mongodb.net/${Config.MONGO_DB}`)
-            .then(() => {
-                logger.info("DB IS CONNECTED");
-                logger.info(`ENTORNO: ${Config.ENVIRONMENT}`);
-            })
-            .catch((error) => {
-                console.log(error);
-                process.exit(4)
-            })
-    }
-
-    //CONFIG
-    app.use(express.json());
-    app.use(express.static("public"))
-    app.use(express.urlencoded({ extended: true }));
-    app.use(cookieParser())
-    initializePassport()
-    app.use(passport.initialize())
-    app.use(addLoger)
-
-    app.use("/apidocs", swaggerUiExpress.serve, swaggerUiExpress.setup(specs))
+} 
+*/
 
 
-    //HANDLEBARS
+const app = express()
 
-    app.engine("handlebars", handlebars.engine())
-    app.set("views", __dirname + "/views")
-    app.set("view engine", "handlebars")
-
-    //ENDPOINTS
-
-    const routerCarts = new CartsRouter()
-    app.use("/api/carts", routerCarts.getRouter())
-
-    const routerProducts = new ProductsRouter()
-    app.use("/api/products", routerProducts.getRouter())
-
-    const routerViews = new ViewsRouter()
-    app.use("/", routerViews.getRouter())
-
-    const routerSessions = new SessionRouter()
-    app.use("/api/sessions", routerSessions.getRouter())
-
-    const routerTickets = new TicketsRouter()
-    app.use("/api/tickets", routerTickets.getRouter())
-
-    const routerUsers = new UsersRouter()
-    app.use("/api/users", routerUsers.getRouter())
-
-    //MOCKING
-    app.get("/mockingproducts", mockingProduct)
-
-    //RENDER PARA TODAS LAS PAGINAS QUE NO EXISTAN
-    app.use("*", (req, res) => {
-        res.render("404", { style: "error.css" })
-    })
-
-    //EVENTOS EN CASO DE QUE UN PROCESO FINALIZE CON UN proccess.exit, Y OTRO EVENTO PARA ATRAPAR ERRORES INESPERADO
-
-    process.on("exit", (code) => {
-        logger.info(`Proceso finalizado con codigo: ${code}`)
-    })
-
-    process.on("uncaughtExeption", exception => {
-        logger.error(`ERROR INESPERADO: ${exception}`);
-    })
-
-    //SOCKET IO
-
-    const httpServer = app.listen(Config.PORT, () => {
-        logger.info(`Servidor corriendo en puerto ${Config.PORT} - WORKER: ${process.pid}`);
-    })
-
-    const io = new Server(httpServer)
-    socket(io)
-
-    // CORRIENDO DB
-
-    environment()
-
+const environment = async () => {
+    await mongoose.connect(`mongodb+srv://${Config.MONGO_USER}:${Config.MONGO_PASSWORD}@coder.amwd2xp.mongodb.net/${Config.MONGO_DB}`)
+        .then(() => {
+            logger.info("DB IS CONNECTED");
+            logger.info(`ENTORNO: ${Config.ENVIRONMENT}`);
+        })
+        .catch((error) => {
+            console.log(error);
+            process.exit(4)
+        })
 }
+
+//CONFIG
+app.use(express.json());
+app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser())
+initializePassport()
+app.use(passport.initialize())
+app.use(addLoger)
+
+app.use("/apidocs", swaggerUiExpress.serve, swaggerUiExpress.setup(specs))
+
+
+//HANDLEBARS
+
+app.engine("handlebars", handlebars.engine())
+app.set("views", __dirname + "/views")
+app.set("view engine", "handlebars")
+
+//ENDPOINTS
+
+const routerCarts = new CartsRouter()
+app.use("/api/carts", routerCarts.getRouter())
+
+const routerProducts = new ProductsRouter()
+app.use("/api/products", routerProducts.getRouter())
+
+const routerViews = new ViewsRouter()
+app.use("/", routerViews.getRouter())
+
+const routerSessions = new SessionRouter()
+app.use("/api/sessions", routerSessions.getRouter())
+
+const routerTickets = new TicketsRouter()
+app.use("/api/tickets", routerTickets.getRouter())
+
+const routerUsers = new UsersRouter()
+app.use("/api/users", routerUsers.getRouter())
+
+//MOCKING
+app.get("/mockingproducts", mockingProduct)
+
+//RENDER PARA TODAS LAS PAGINAS QUE NO EXISTAN
+app.use("*", (req, res) => {
+    res.render("404", { style: "error.css" })
+})
+
+//EVENTOS EN CASO DE QUE UN PROCESO FINALIZE CON UN proccess.exit, Y OTRO EVENTO PARA ATRAPAR ERRORES INESPERADO
+
+process.on("exit", (code) => {
+    logger.info(`Proceso finalizado con codigo: ${code}`)
+})
+
+process.on("uncaughtExeption", exception => {
+    logger.error(`ERROR INESPERADO: ${exception}`);
+})
+
+//SOCKET IO
+
+const httpServer = app.listen(Config.PORT, () => {
+    logger.info(`Servidor corriendo en puerto ${Config.PORT} - WORKER: ${process.pid}`);
+})
+
+const io = new Server(httpServer)
+socket(io)
+
+// CORRIENDO DB
+
+environment()
+
